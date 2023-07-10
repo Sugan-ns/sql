65714540e84876f3351e2dddc45ab421de79ab47
Sugan-ns <139169332+Sugan-ns@users.noreply.github.com>
2023-07-11 00:40:44 +0530

    Add files via upload

---
+++ sql.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: select*from b_customers order by b_id
+LIMIT 0, 300
+
+-- Date: 2023-07-11 00:30
+*/
+INSERT INTO `` (`B_id`,`accountNumber`,`pin`,`c_name`,`balance`,`acc_type`,`c_addres`) VALUES (1,123,234,'tharun',4567899,'savings','harur');
+INSERT INTO `` (`B_id`,`accountNumber`,`pin`,`c_name`,`balance`,`acc_type`,`c_addres`) VALUES (2,234,9876,'raja',7654,'saving','salem');
+INSERT INTO `` (`B_id`,`accountNumber`,`pin`,`c_name`,`balance`,`acc_type`,`c_addres`) VALUES (3,23,2345,'vijai',374890,'saving','salem');
+INSERT INTO `` (`B_id`,`accountNumber`,`pin`,`c_name`,`balance`,`acc_type`,`c_addres`) VALUES (4,456,3456,'rani',364758,'saving','manaparai');
+INSERT INTO `` (`B_id`,`accountNumber`,`pin`,`c_name`,`balance`,`acc_type`,`c_addres`) VALUES (5,556,678,'vishal',2134,'saving','salem');
+INSERT INTO `` (`B_id`,`accountNumber`,`pin`,`c_name`,`balance`,`acc_type`,`c_addres`) VALUES (6,65738,698,'praveen',9866,'saving','salem');
+INSERT INTO `` (`B_id`,`accountNumber`,`pin`,`c_name`,`balance`,`acc_type`,`c_addres`) VALUES (7,748,9647,'surya',7685930,'saving','salem');
+INSERT INTO `` (`B_id`,`accountNumber`,`pin`,`c_name`,`balance`,`acc_type`,`c_addres`) VALUES (8,897,86975,'sujitha',7689,'saving','kochin');
+INSERT INTO `` (`B_id`,`accountNumber`,`pin`,`c_name`,`balance`,`acc_type`,`c_addres`) VALUES (9,9875,8679,'raja',465768,'saving','salem');
+INSERT INTO `` (`B_id`,`accountNumber`,`pin`,`c_name`,`balance`,`acc_type`,`c_addres`) VALUES (10,1099,3759,'karthi',86749,'saving','harur');
